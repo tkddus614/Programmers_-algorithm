46733f6132ca62c166e93f33020b369701caf241
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2023-12-02 18:01:11 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let sum = 0;
+    if (n % 2 === 0) {
+        for (let i = 2; i <= n; i+=2) {
+            console.log(i)
+            sum += i*i
+        }
+    } else {
+        for (let i = 1; i <= n; i+=2) {
+            sum += i
+        }
+    }
+    return sum;
+}
