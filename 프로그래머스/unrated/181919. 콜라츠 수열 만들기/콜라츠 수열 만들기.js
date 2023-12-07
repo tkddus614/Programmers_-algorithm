3a989d255bc6600d29f059ba5063027f3b1b4564
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2023-12-07 20:28:17 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let answer = [];
+    while (n !== 1) {
+        answer.push(n);
+        n = n % 2 === 0 ? n / 2 : 3 * n + 1
+    } 
+  answer.push(1)
+  return answer
+}
