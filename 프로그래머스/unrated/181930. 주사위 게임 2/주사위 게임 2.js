e4e7223e18462de1117928c2cfb65c5ea2a10bd1
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2023-12-10 18:11:44 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,9 @@
+function solution(a, b, c) {
+    if (a !== b && b !== c && a !== c) {
+      return a + b + c;
+    } else if (a === b && b === c && a === c) {
+      return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    } else {
+      return (a + b + c) * (a**2 + b**2 + c**2)
+    }
+}
