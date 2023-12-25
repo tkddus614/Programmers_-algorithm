2af42c9ad4d3c2b7ebb063a0977c34e1973ff6e8
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2023-12-25 17:34:11 +0900

    [level unrated] Title: 배열 만들기 4, Time: 2.87 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    const stk = [];
+    let i = 0;
+  
+    while (i < arr.length) {
+      if (stk.length === 0) {
+        stk.push(arr[i])
+        i++
+      } else if (stk.length !== 0) {
+        if (stk[stk.length - 1] < arr[i]) {
+          stk.push(arr[i])
+          i++
+        } else if (stk[stk.length - 1] >= arr[i]) {
+          stk.pop()
+        }
+      }
+    }
+    return stk
+}
