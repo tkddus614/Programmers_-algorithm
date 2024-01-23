d96af3bae2335685309918ac5a11b98d5beb52e2
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2024-01-23 20:54:54 +0900

    [level 1] Title: 삼총사, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,11 @@
+function solution(number) {
+    let count = 0;
+    for(i=0;i<number.length;i++) {
+      for(j=i+1;j<number.length;j++) {
+        for(k=j+1;k<number.length;k++) {
+          if (number[i] + number[j] + number[k] === 0) count++
+        }
+      }
+    }
+    return count
+}
