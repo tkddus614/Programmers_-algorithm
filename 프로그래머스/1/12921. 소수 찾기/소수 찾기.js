c3beacc4086f30bc728fda0ae1f390a6c219677c
Sangyeon Kim <73007012+tkddus614@users.noreply.github.com>
2024-02-06 20:57:27 +0900

    [level 1] Title: 소수 찾기, Time: 30.14 ms, Memory: 45.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let arr = Array(n + 1).fill(true).fill(false, 0, 2)
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  return arr.filter((v) => v).length
+}
